c7afea56379f90609de227fb04070df3bcba2e9f
jfang2121 <jfang@bates.edu>
2024-05-28 10:05:46 -0500

    csv link

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,5 @@
 // Then Publish the Google Sheet as csv
 
 // paste in your published Google Sheets URL from the browser address bar
-https://docs.google.com/spreadsheets/d/e/2PACX-1vSo8zAjfZ0Ep8ShroN0-S-3J_g-P29un6MMW2rERBcTp8BSVEtIbrUKQceXuYWMfktOgA-iV1wadsb6/pub?output=csv 
+
+https://docs.google.com/spreadsheets/d/e/2PACX-1vTw5t4Ea9oSMp98mNQooQSxjJtaEPgvmxT1lqY9JyUuRY-STK28MZ0VHqKhpQPEpunCULcSxF3vcG5u/pub?output=csv
